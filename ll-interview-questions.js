bf775625cca78c16fdf3cd2f51a0c90d86031169
talexcrowell <talexcrowell@gmail.com>
2018-12-13 14:11:01 -0800

    cycle check completed

--- ll-interview-questions.js
+++ ll-interview-questions.js
@@ -17,7 +17,7 @@ function WhatDoesThisProgramDo(lst){
   }
 }
 
-// this program checks to see if there are any duplicates within the linked list
+// this program checks to see if there are any duplicates within the linked list and removes them
 // O(n^2) because for every time the current node is moved, the new node has to iterate through the next nodes and compare values to current node's value and THEN take action (.next vs .next.next). 
 // once the new node's value is null, the nested while loop will break and then the process will repeat until current node's value is null.
 
@@ -81,3 +81,53 @@ function thirdFromTheEnd(list){
     }
   }
 }
+
+
+//middle of the list
+//input:  HEAD => A -> B -> C -> D -> E -> F -> G -> null    <----  LL
+//output: D
+
+//Create two temp nodes, current and twoTimesAfter
+//Start values: current = HEAD, twoTimesAfter = current.next.next;
+//When twoTimesAfter === null, we return the value of our current node
+
+function middleOfTheList(list){
+  let current = list.head;
+  let twoTimesAfter = current.next;
+  while(twoTimesAfter !== null){
+    current = current.next;
+    twoTimesAfter = twoTimesAfter.next.next;
+    if(twoTimesAfter === null){
+      return current.value;
+    }
+  }
+}
+
+//cycle in the list
+//input:  HEAD => A -> B -> C -> D -> C -> D -> C -> etc.    <----  LL
+//output: true
+
+//Create two temp nodes, current, after, further
+//Starting values: current = HEAD, after = current.next
+//If after or further === null, then return false
+//If after or further === current.value, then return true
+
+function cycleCheck(list){
+  let current = list.head;
+  let after = current.next;
+  let further = current.next.next;
+  while(current !== null){
+    current = current.next;
+    after = after.next;
+    further= further.next.next;
+    if(after === null || further === null){
+      return false;
+    }
+    //check for loop by comparing values of after and further to current.value and further with after.value to make sure a value didnt get skipped if there is a loop
+    if(after === current.value || further === current.value || further === after.value){
+      return true;
+    }
+  }
+}
+
+
